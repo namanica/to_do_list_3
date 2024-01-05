d6473481c930a4c85bbbc5d45e8ab52fa35f1b31
namanica <veronichkapilipna@gmail.com>
2024-01-05 22:13:51 +0200

    fixed an error

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,25 +1,31 @@
+import { ITask } from '@/types/tasks'
 import React from 'react'
 
-const TodoList = () => {
+interface TodoListProps {
+  tasks: ITask[]
+}
+
+const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
+  console.log(tasks);
   return (
     <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Favorite Color</th>
-      </tr>
-    </thead>
-    <tbody>
-      {/* row 1 */}
-      <tr>
-        <td>Cy Ganderton</td>
-        <td>Blue</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+      <table className="table w-full">
+        <thead>
+          <tr>
+            <th>Tasks</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <tr key={task?.id}>
+            <td>{task?.text}</td>
+            <td>Blue</td>
+          </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
