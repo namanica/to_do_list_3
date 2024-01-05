ccea5b67467867a779897f0a2b388edca7bd1e75
namanica <veronichkapilipna@gmail.com>
2024-01-05 18:35:39 +0200

    added main panel

---
+++ app/components/AddTask.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaPlus } from "react-icons/fa"
+
+const AddTask = () => {
+  return (
+    <div>
+      <button className='btn btn-primary w-full'>Add new task
+      <FaPlus /></button>
+    </div>
+  )
+}
+
+export default AddTask
