b1c27291da70d1f8cd0d7db7a5512cf695f59ed4
namanica <veronichkapilipna@gmail.com>
2024-01-06 15:00:28 +0200

    added input

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,17 +1,31 @@
 'use client';
-import React, { useState } from 'react'
+import React, { FormEventHandler, useState } from 'react'
 import { FaPlus } from "react-icons/fa"
 import Modal from './Modal'
 
 const AddTask = () => {
   const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [newTask, addNewTask] = useState<string>('');
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    console.log(newTask);
+    addNewTask("");
+  }
   return (
     <div>
       <button onClick={() => setModalOpen(true)} className='btn btn-primary w-full'>Add new task
       <FaPlus /></button>
 
       <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
-        modal for adding the task
+        <form onSubmit={handleSubmit}>
+          <h3 className='font-bold '>
+            add new task
+          </h3>
+          <div className='modal-action'>
+          <input value={newTask} onChange={e => addNewTask(e.target.value)} type="text" placeholder="what are you going to do?" className="input input-bordered w-full" />
+          <button type="submit" className='btn'>add</button>
+          </div>
+        </form>
       </Modal>
     </div>
   )
