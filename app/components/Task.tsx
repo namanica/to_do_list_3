c9b188e4e543ab0fb48a4f01f1dfa359973c9452
namanica <veronichkapilipna@gmail.com>
2024-01-06 18:24:05 +0200

    made editing functionality

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,17 +1,49 @@
-import React from 'react'
+'use client';
+import React, { FormEventHandler, useState } from 'react'
 import { ITask } from '@/types/tasks'
 import { FaEdit } from "react-icons/fa";
 import { FaTrash } from "react-icons/fa6";
+import Modal from './Modal';
+import { useRouter } from 'next/navigation';
+import { editTodo } from '@/api';
 interface TaskProps {
   task: ITask;
 }
 
 const Task: React.FC<TaskProps> = ( { task } ) => {
+  const router = useRouter();
+  const [openModalEdit, setOpenModalEdit] = useState<boolean>(false);
+  const [openModalDelete, setOpenModalDelete] = useState<boolean>(false);
+  const [taskToEdit, setTaskToEdit] = useState<string>(task.text);
+
+  const handleSubmitEdit : FormEventHandler<HTMLFormElement> =
+  async (e) => {
+    e.preventDefault();
+    await editTodo({
+      id: task.id,
+      text: taskToEdit,
+    })
+    setTaskToEdit("");
+    setOpenModalEdit(false);
+    router.refresh();
+  }
+
   return (
     <tr key={task.id}>
     <td className='w-full'>{task.text}</td>
     <td className='flex gap-5'>
-    <FaEdit cursor="pointer" size={20} />
+    <FaEdit onClick={() => setOpenModalEdit(true)} cursor="pointer" size={20} />
+    <Modal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
+        <form onSubmit={handleSubmitEdit}>
+          <h3 className='font-bold '>
+            update the task
+          </h3>
+          <div className='modal-action'>
+          <input value={taskToEdit} onChange={e => setTaskToEdit(e.target.value)} type="text" placeholder="what are you going to do?" className="input input-bordered w-full" />
+          <button type="submit" className='btn'>update</button>
+          </div>
+        </form>
+    </Modal>
     <FaTrash cursor="pointer" size={20} />
     </td>
     </tr>
