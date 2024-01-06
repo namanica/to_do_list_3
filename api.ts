95f26bf84fd487d53a8612f1ad2f9b2c6c52c85e
namanica <veronichkapilipna@gmail.com>
2024-01-06 15:13:57 +0200

    connected the added tasks with api

--- api.ts
+++ api.ts
@@ -8,3 +8,13 @@ export const getAllTodos = async (): Promise<ITask[]> => {
   const todos = await res.json();
   return todos;
 }
+
+export const addTodo = async (todo: ITask): Promise<ITask> => {
+  const res = await fetch(`${baseUrl}/tasks`, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(todo),
+  });
+  const newTodo = await res.json();
+  return newTodo;
+}
